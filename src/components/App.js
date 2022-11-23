1c42c5adef25c5d234b57689dc886cc6725fed8c
Celia Martin Herranz <celiamartinherranz11@gmail.com>
2022-11-23 14:07:43 +0100

    add event cards open and closed with react

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,10 @@ function App() {
     github: '',
   });
 
+  const [designIsOpen, setDesignIsOpen] = useState (false);
+  const [fillIsOpen, setFillIsOpen] = useState (false);
+  const [shareIsOpen, setShareIsOpen] = useState (false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
@@ -25,6 +29,221 @@ function App() {
     setUser({ ...user, [inputName]: inputValue });
     console.log(user);
   };
+
+  const handleToggleDesign = () => {
+    setDesignIsOpen(!designIsOpen);
+  };
+
+  const handleToggleFill = () => {
+    setFillIsOpen(!fillIsOpen);
+  };
+
+  const handleToggleShare = () => {
+    setShareIsOpen(!shareIsOpen);
+  };
+
+  const renderDesign = () => {
+    if (designIsOpen === true) {
+      return(<section className="largeContainerDesign  js-design">
+      <div className="mediumContainerDesign">
+        <div className="boxDesignColorsLabel">
+          <span className="colorsText">Colores</span>
+        </div>
+        <div className="colorPalette__container1">
+          <input
+            id="palette1"
+            className="colorPalette__input1 js-palette1"
+            type="radio"
+            value="1"
+            name="palette"
+            checked={user.palette === '1'}
+            onChange={handleInput}
+          />
+          <section className="colorsBoxOne">
+            <div className="rectangle__a1"></div>
+            <div className="rectangle__a2"></div>
+            <div className="rectangle__a3"></div>
+          </section>
+        </div>
+      </div>
+
+      <div className="colorPalette__container2">
+        <input
+          id="palette2"
+          className="js-palette2 colorPalette__input2 "
+          type="radio"
+          value="2"
+          name="palette"
+          checked={user.palette === '2'}
+          onChange={handleInput}
+        />
+        <section className="colorsBoxTwo">
+          <div className="rectangle__b1"></div>
+          <div className="rectangle__b2"></div>
+          <div className="rectangle__b3"></div>
+        </section>
+      </div>
+
+      <div className="colorPalette__container3">
+        <input
+          id="palette3"
+          className="js-palette3 colorPalette__input3"
+          type="radio"
+          value="3"
+          name="palette"
+          checked={user.palette === '3'}
+          onChange={handleInput}
+        />
+        <section className="colorsBoxThree">
+          <div className="rectangle__c1"></div>
+          <div className="rectangle__c2"></div>
+          <div className="rectangle__c3"></div>
+        </section>
+      </div>
+    </section>)
+    }
+  };
+
+  const renderFill = () => {
+    if (fillIsOpen === true) {
+      return(<div action="" className="fill js-fill">
+      <label htmlFor="full_name" className="fill__infoLabel">
+        Nombre completo
+      </label>
+      <input
+        type="text"
+        id="full_name"
+        name="name"
+        className="fill__infoInput js_input_name js_inputReset"
+        placeholder="Nombre Apellido"
+        required
+        autoComplete="name"
+        value={user.name}
+        onInput={handleInput}
+      />
+
+      <label htmlFor="job" className="fill__infoLabel">
+        Puesto
+      </label>
+      <input
+        type="text"
+        id="job"
+        name="job"
+        className="fill__infoInput js_input_job js_inputReset"
+        placeholder="Full stack developer"
+        required
+        value={user.job}
+        onInput={handleInput}
+      />
+
+      <label htmlFor="imageForm" className="fill__infoLabel">
+        Imagen de perfil
+      </label>
+      <div className="fill__action ">
+        <label
+          htmlFor="imageForm"
+          className="fill__infoLabel fill__action--labelInput"
+        >
+          Añadir imagen
+        </label>
+        <input
+          type="file"
+          accept="image/*"
+          className="fill__infoInput fill__action--imageInput js_input_img js__profile-upload-btn js_inputReset"
+          id="imageForm"
+          name="img"
+        />
+        <div className="fill__action--preview js__profile-preview">
+          {' '}
+        </div>
+      </div>
+
+      <label htmlFor="email" className="fill__infoLabel">
+        Email
+      </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        className="fill__infoInput js_input_email js_inputReset"
+        placeholder="ejemplo@dominio.tld"
+        autoComplete="email"
+        required
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+        value={user.email}
+        onInput={handleInput}
+      />
+      <small className="fill__small js-small-text"></small>
+
+      <label htmlFor="phone" className="fill__infoLabel">
+        Teléfono
+      </label>
+      <input
+        type="tel"
+        id="phone"
+        name="phone"
+        className="fill__infoInput js_input_phone js_inputReset"
+        placeholder="XXXXXXXXX"
+        autoComplete="tel"
+        required
+        pattern="[6-9]{1}[0-9]{8}"
+        value={user.phone}
+        onInput={handleInput}
+      />
+      <small className="fill__small js-small-text"></small>
+
+      <label htmlFor="linkedin" className="fill__infoLabel">
+        Linkedin
+      </label>
+      <input
+        type="text"
+        id="linkedin"
+        name="linkedin"
+        className="fill__infoInput js_input_linkedin js_inputReset"
+        placeholder="usuario"
+        autoComplete="url"
+        value={user.linkedin}
+        onInput={handleInput}
+      />
+
+      <label htmlFor="github" className="fill__infoLabel">
+        Github
+      </label>
+      <input
+        type="text"
+        id="github"
+        name="github"
+        className="fill__infoInput js_input_github js_inputReset"
+        placeholder="usuario"
+        autoComplete="url"
+        value={user.github}
+        onInput={handleInput}
+      />
+    </div>)}};
+
+    const renderShare = () => {
+      if (shareIsOpen === true) {
+        return(<section className="buttonComparte js-share">
+        <a href="" className="linkComparte js-linkCreated">
+          <i className="fa-regular fa-address-card"></i>Crear Tarjeta
+        </a>
+        <article className="cardCreated js-cardCreated hidden js-share">
+          <h3 className="cardCreated_text js-textError"></h3>
+          <a
+            className="cardCreated_link js-shareUrl"
+            href=""
+            target="_blank"
+          ></a>
+          <a
+            className="cardCreated_button js-twitter hidden"
+            target="_blank"
+          >
+            <i className="fa-brands fa-twitter" href=""></i>Compartir en
+            twitter
+          </a>
+        </article>
+      </section>)}};
+
   return (
     <div>
       {/* HEADER */}
@@ -84,7 +303,7 @@ function App() {
         <form className="mainCreate__design" onSubmit={handleSubmit}>
           {/* DISEÑA */}
           <fieldset className="design">
-            <div className="buttonDesign js-designClick">
+            <div className="buttonDesign js-designClick" onClick={handleToggleDesign}>
               <legend className="buttonDesign__title">
                 <i className="fa-solid fa-object-ungroup"></i> Diseña{' '}
               </legend>
@@ -94,7 +313,8 @@ function App() {
                 alt=""
               />
             </div>
-
+            {renderDesign()}
+{/*  
             <section className="largeContainerDesign  js-design">
               <div className="mediumContainerDesign">
                 <div className="boxDesignColorsLabel">
@@ -152,11 +372,12 @@ function App() {
                 </section>
               </div>
             </section>
+            */}
           </fieldset>
 
           {/* RELLENA */}
           <fieldset>
-            <div className="buttonFill js-fillClick">
+            <div className="buttonFill js-fillClick" onClick={handleToggleFill}>
               <legend className="buttonFill__title">
                 <i className="fa-regular fa-keyboard"></i>Rellena
               </legend>
@@ -166,7 +387,8 @@ function App() {
                 alt=""
               />
             </div>
-
+            {renderFill()}
+{/*  
             <div action="" className="fill js-fill">
               <label htmlFor="full_name" className="fill__infoLabel">
                 Nombre completo
@@ -281,11 +503,12 @@ function App() {
                 onInput={handleInput}
               />
             </div>
+            */}
           </fieldset>
 
           {/* COMPARTE */}
           <fieldset className="share">
-            <section className="buttonShare js-shareClick">
+            <section className="buttonShare js-shareClick" onClick={handleToggleShare}>
               <p className="buttonShare__title">
                 <i className="fa-solid fa-share-nodes"></i> Comparte{' '}
               </p>
@@ -295,7 +518,8 @@ function App() {
                 alt=""
               />
             </section>
-
+            {renderShare()}
+{/*  
             <section className="buttonComparte js-share">
               <a href="" className="linkComparte js-linkCreated">
                 <i className="fa-regular fa-address-card"></i>Crear Tarjeta
@@ -316,6 +540,7 @@ function App() {
                 </a>
               </article>
             </section>
+            */}
           </fieldset>
         </form>
       </main>
