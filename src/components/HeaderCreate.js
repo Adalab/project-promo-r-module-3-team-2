17a9e57c0e4f84c1a731b4fb9813ccd1e236f87d
Calpurniax <sofia.gracia@gmail.com>
2022-11-29 13:27:51 +0100

    create component Header

---
+++ src/components/HeaderCreate.js
@@ -0,0 +1,16 @@
+import image from '../images/title.png';
+import '../styles/components/HeaderCreate.scss';
+const HeaderCreate = () => {
+    return (
+        <header className="headerCreate">
+            <a href="#">
+                <img
+                    src={image}
+                    className="headerCreate__img"
+                    alt="Logo Awesome profile-cards"
+                />
+            </a>
+        </header>
+    )
+};
+export default HeaderCreate;
